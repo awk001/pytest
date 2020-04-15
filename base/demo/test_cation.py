2c870670c0be0fbc6eb352a72e1b0a4e1c8102a9
awk001 <anwemkang7048@163.com>
2020-04-15 12:01:08 +0800

    commit

---
+++ base/demo/test_cation.py
@@ -0,0 +1,20 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+d = webdriver.Firefox()
+d.get("http://www.baidu.com")
+d.implicitly_wait(10)
+d.maximize_window()
+element = d.find_element(By.ID, "kw")
+action = ActionChains(d)
+# action.send_keys("selenium")
+# action.send_keys_to_element(d.find_element(By.ID,"kw"),"selenium",Keys.ENTER)
+# 按下shift键输入的字母为大写
+action.key_down(Keys.SHIFT).send_keys("abc").key_up(Keys.SHIFT).send_keys("abc")  # ABCabc
+action.perform()
+sleep(3)
+
+d.quit()
---
+++ base/demo/test_js.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://www.baidu.com")
+driver.implicitly_wait(10)
+
+driver.find_element_by_id("kw").send_keys('selenium')
+driver.find_element_by_id("su").click()
+driver.find_element_by_link_text("Selenium_百度百科").click()
+
+# jsStr = "return document.title"
+# script = driver.execute_script(jsStr)
+# print(script)
+element = driver.find_element(By.ID, "kw")
+jsStr = "arguments[0].setAttribute('style',arguments[1]);"
+driver.execute_script(jsStr,element,"color:orange;border:4px solid orange;")
+
+time.sleep(3)
+driver.quit()
